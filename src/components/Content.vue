69bb7b3a79944c73f683193f9361a4a1ef8de084
bartu <bartu.oeztuerk@student.htw-berlin.de>
2021-11-30 12:52:02 +0100

    Was auf der Seite zu sehen ist

---
+++ src/components/Content.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="content">
+    <h3>This is {{mode}} mode</h3>
+    <div class="image-grid">
+    </div>
+    <p class="para">
+     Hallo Malik
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['mode']
+}
+</script>
+
+<style>
+.content {
+  padding: 25px;
+}
+.content h3 {
+  font-size: 28px;
+  text-transform: uppercase;
+  font-weight: 300;
+  margin-bottom: 25px;
+}
+.image-grid {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 15px;
+}
+.image-grid .col img {
+  width: 100%;
+}
+.para {
+  width: 100%;
+  max-width: 600px;
+  margin: 25px auto 0px;
+}
+</style>
