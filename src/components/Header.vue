9ebf29c2b8a41a0345c69031900f5877885b8419
bartu <bartu.oeztuerk@student.htw-berlin.de>
2021-12-01 10:11:49 +0100

    Login/ Sign Up und Shopping List als Titel in der Navbar

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,21 +1,36 @@
 <template>
   <header>
-    <h1>{{ mode }} Mode</h1>
+    <h1>Shopping List</h1>
     <Toggle :mode="mode" @toggle="$emit('toggle')" />
     <nav>
       <div class="nav-link">Home</div>
       <div class="nav-link">About</div>
-      <div class="nav-link">Portfolio</div>
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-          Dropdown
+          Login/Sign Up
         </a>
-        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <li><a class="dropdown-item" href="#">Action</a></li>
-          <li><a class="dropdown-item" href="#">Another action</a></li>
-          <li><hr class="dropdown-divider"></li>
-          <li><a class="dropdown-item" href="#">Something else here</a></li>
-        </ul>
+        <div class="nav-item dropdown-menu">
+          <form class="px-4 py-3">
+            <div class="form-group">
+              <label for="exampleDropdownFormEmail1">Email address</label>
+              <input type="email" class="form-control" id="exampleDropdownFormEmail1" placeholder="email@example.com">
+            </div>
+            <div class="form-group">
+              <label for="exampleDropdownFormPassword1">Password</label>
+              <input type="password" class="form-control" id="exampleDropdownFormPassword1" placeholder="Password">
+            </div>
+            <div class="form-check">
+              <input type="checkbox" class="form-check-input" id="dropdownCheck">
+              <label class="form-check-label" for="dropdownCheck">
+                Remember me
+              </label>
+            </div>
+            <button type="submit" class="btn btn-primary">Log in</button>
+          </form>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">New around here? Sign up</a>
+          <a class="dropdown-item" href="#">Forgot password?</a>
+        </div>
       </li>
     </nav>
   </header>
