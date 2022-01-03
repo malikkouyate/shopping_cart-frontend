0c05f2ca1a93ce49ce1a9256590f1c92d260c1d5
bartu <bartu.oeztuerk@student.htw-berlin.de>
2022-01-03 12:38:23 +0100

    Frontend für registrieren, Terms of Service hinzugefügt

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import About from '../views/About.vue'
 import Object from '../views/Items.vue'
+import Register from '../views/Register'
+import TermsofService from '../views/TermsofService'
 
 const routes = [
   {
@@ -18,6 +20,16 @@ const routes = [
     path: '/object',
     name: 'Object',
     component: Object
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: Register
+  },
+  {
+    path: '/termsofservice',
+    name: 'TermsofService',
+    component: TermsofService
   }
 ]
 
